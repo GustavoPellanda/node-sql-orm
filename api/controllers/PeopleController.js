e953e8f167ece49b591dceb628beff490abb3a75
Gustavo Pellanda <gusfipe@gmail.com>
2023-10-20 21:11:12 -0300

    Get Person method added.

--- api/controllers/PeopleController.js
+++ api/controllers/PeopleController.js
@@ -1,14 +1,27 @@
 const database = require("../models");
 
-class PeopleController{
-    static async getAllPeople(req, res){
-        try {
-            const allPeople = await database.People.findAll();
-            return res.status(200).json(allPeople);
-        } catch (error) {
-            return res.status(500).json(error.message);
-        }
+class PeopleController {
+  static async getPeople(req, res) {
+    try {
+      const people = await database.People.findAll();
+      return res.status(200).json(people);
+    } catch (error) {
+      return res.status(500).json({ error: 'Internal Server Error', message: error.message });
     }
+  }
+
+  static async getPerson(req, res) {
+    const { id } = req.params; // Ensure id is an int
+    try {
+      const person = await database.People.findOne({ where: { id: Number(id) } }); // Where the ID of the database is the ID of the req.params
+      if (!person) {
+        return res.status(404).json({ error: 'Not Found', message: 'Person not found' });
+      }
+      return res.status(200).json(person);
+    } catch (error) {
+      return res.status(500).json({ error: 'Internal Server Error', message: error.message });
+    }
+  }
 }
 
 module.exports = PeopleController;
--- api/routes/peopleRoute.js
+++ api/routes/peopleRoute.js
@@ -3,6 +3,7 @@ const PeopleController = require('../controllers/PeopleController');
 
 const router = Router();
 
-router.get('/people', PeopleController.getAllPeople);
+router.get('/people', PeopleController.getPeople);
+router.get('/people/:id', PeopleController.getPerson);
 
 module.exports = router;
