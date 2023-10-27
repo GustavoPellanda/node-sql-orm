9cd4d98cf933ef59f1682e305d4d1dd35630fdb8
Gustavo Finau Pellanda <129123498+GustavoPellanda@users.noreply.github.com>
2023-10-27 10:01:28 -0300

    Addresses and Orders routes created.

--- api/routes/addressesRoute.js
+++ api/routes/addressesRoute.js
@@ -1,6 +1,12 @@
 const { Router } = require('express');
 const AddressesController = require('../controllers/AddressesController');
 
+router.get('/address', AddressesController.getOneAddress);
+router.get('/address/:id', AddressesController.getAllAddresses);
+router.post('/address', AddressesController.createAddress);
+router.get('/address/:id', AddressesController.changeAddress);
+router.get('/address/:id', AddressesController.deleteAddress);
+
 const router = Router();
 
 module.exports = router;
