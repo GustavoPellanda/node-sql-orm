c13b18319b8764c4d59ac0b24564e1a3078e2bc7
Gustavo Finau Pellanda <129123498+GustavoPellanda@users.noreply.github.com>
2023-10-27 10:08:06 -0300

    Addresses and Orders routes created.

--- api/routes/ordersRoute.js
+++ api/routes/ordersRoute.js
@@ -3,4 +3,10 @@ const OrdersController = require('../controllers/OrdersController');
 
 const router = Router();
 
+router.get('/order', OrdersController.getAllOrders);
+router.get('/people/:id/order/:id', OrdersController.getOneOrder);
+router.post('/order', OrdersController.createOrder);
+router.patch('/people/:id/order/:id', OrdersController.changeOrder);
+router.delete('/people/:id/order/:id', OrdersController.deleteOrder);
+
 module.exports = router;
